fcf8ffd274414f2e6c0d9b025e6fedf2915b4bfd
Sindhuja-G <sindhuja.golagani@techolution.com>
2026-02-03 18:21:01 +0530

    Commit changes

--- Programs/P01_hello.py
+++ Programs/P01_hello.py
@@ -7,7 +7,9 @@ def justPrint(text):
     base_value = 10
     increment_value=20
     difference = increment_value - base_value
+    divide_value = increment_value / base_value
     print("Difference is:", difference)
+    print("Divide value is:", divide_value)
 
 if __name__ == '__main__':
     justPrint('Hello Sindhuja')
