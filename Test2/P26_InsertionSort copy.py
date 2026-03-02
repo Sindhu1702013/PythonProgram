da4e39c7cc3afe615d8f8061d649a7d25d9a45a8
Sindhu17020103 <sindhuja.golagani@techolution.com>
2026-03-02 16:08:17 +0530

    Added changes

--- Test2/P26_InsertionSort copy.py
+++ Test2/P26_InsertionSort copy.py
@@ -11,12 +11,11 @@
 def insertionSort(List):
     for i in range(1, len(List)):
         currentNumber = List[i]
-        for j in range(i - 1, -1, -1):
-            if List[j] > currentNumber :
-                List[j], List[j + 1] = List[j + 1], List[j]
-            else:
-                List[j + 1] = currentNumber
-                break
+        j = i - 1
+        while j >= 0 and List[j] > currentNumber:
+            List[j + 1] = List[j]
+            j -= 1
+        List[j + 1] = currentNumber
 
     return List
 
