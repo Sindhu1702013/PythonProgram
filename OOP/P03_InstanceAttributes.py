05806ad1035652af08f32eb2585e51ce1514df52
Sindhuja Golagani <sindhuja.golagani@techolution.com>
2026-03-02 19:29:59 +0530

    Add randomValue instance attribute to Vehicle class

--- OOP/P03_InstanceAttributes.py
+++ OOP/P03_InstanceAttributes.py
@@ -14,3 +14,15 @@ class Vehicle():
 car = Vehicle()
 car.type()              #Calling the class Method
 print(car.randomValue)  #Calling the instance attribute
+
+import random
+class Vehicle():
+    #Class Methods/ Attributes
+    def type(self):
+        #NOTE: This is not a class attribute as the variable is binded to self. Hence it becomes
+        #instance attribute
+        self.randomValue = random.randint(1,10) #Setting the instance attribute
+
+car = Vehicle()
+car.type()              #Calling the class Method
+print(car.randomValue)  #Calling the instance attribute
